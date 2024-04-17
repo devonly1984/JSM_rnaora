3bd254fc9e8ad652b4280210b815c5270675b24c
unknown <PCGUARD@PON2027486D.safeway01.ad.safeway.com>
2024-04-16 20:58:36 -0500

    1:29:15

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,16 +3,21 @@ import {images} from '../../constants'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FormField from '../../components/FormField';
 import { useState } from 'react';
-
+import CustomButton from "../../components/CustomButton";
+import { Link } from 'expo-router';
 const SignIn = () => {
   const [form, setForm] = useState({
     email: '',
     password:''
   })
+  const [isSumitting, setisSumitting] = useState(false)
+  const submit = ()=>{
+
+  }
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full px-6 my-6">
+        <View className="w-full justify-center min-h-[80vh] px-6 my-6">
           <Image
             source={images.logo}
             resizeMode="contain"
@@ -34,6 +39,23 @@ const SignIn = () => {
             handleChangeText={(e) => setForm({ ...form, password: e })}
             otherStyles="mt-7"
           />
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSumitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Dont have an account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg text-secondary font-psemibold"
+            >
+              Sign Up
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -35,6 +35,9 @@ NativeWindStyleSheet.setOutput({
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/*<Stack.Screen name="/search/[query]" options={{ headerShown: false }} />*/}
     </Stack>
   );
 };
